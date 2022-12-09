dac518dc596b11a8fecb5cd1ca759c812981c0ee
Gergo Vamosi <geritwo@gmail.com>
2022-12-09 18:35:52 +0100

    Separate movement direction updates

--- player.py
+++ player.py
@@ -36,8 +36,14 @@ class Player(pygame.sprite.Sprite):
     def move(self, dt):
         if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
+
+        # Horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        # Vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
 
     def update(self, dt):
         self.input()
